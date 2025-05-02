d44b03e503da41bea920954c6774f354ff59d156
MH-liton-360 <liton72524nk@gmail.com>
2025-05-02 23:25:29 +0600

    Footer Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,60 +1,56 @@
 import logo from '../assets/ctg-logo.jpg'
-import github from '../assets/github-logo-png-transparent-png.png'
+import { FaFacebookF, FaInstagram, FaGithub, FaXTwitter } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
-        <footer className="footer footer-horizontal footer-center bg-[#0c0c0cc9] text-primary-content p-10">
-            <aside className='p-1'>
-                <img
-                    width="50"
-                    height="50"
-                    src={logo}
-                    alt="Logo"
-                    className="inline-block rounded-2xl"
-                />
-                <p className="font-bold p-2">
-                    Car-Cleaning LTD.
-                    <br />
-                    Providing reliable tech since 2025
-                </p>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-            </aside>
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <a href='https://github.com/programming-hero-web-course1/b10-a9-authentication-MH-liton-360'>
-                        <img
-                            src={github}
-                            alt="GitHub"
-                            width="24"
-                            height="24"
-                            className='rounded-2xl'
-                        />
-                    </a>
-                    <a href='https://www.youtube.com/'>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                        </svg>
-                    </a>
-                    <a href='https://www.facebook.com/MH.liton.360'>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                        </svg>
-                    </a>
+        <footer className="bg-[#27323f] text-white py-10 px-6 md:px-16 lg:px-24">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-15">
+
+                <div>
+                    <div className='flex text-center items-center'>
+                        <img className='h-[50px] rounded-2xl' src={logo} alt="" />
+                        <p className='text-center'><strong className='text-amber-50 px-1'>mar<strong className='text-amber-500'>k</strong>et</strong></p>
+                    </div>
+                    <p className="mb-2">Location: Dhaka, Bangladesh</p>
+                    <p className="mb-2">Phone: +8801758-908138</p>
+                    <p className="mb-2">Email: liton72524nk@gmail.com</p>
+                    <p className="mb-4">Opening hours: 9.00 AM - 5.00 PM</p>
+                    <div className="flex gap-3 text-xl">
+                        <a href="https://www.facebook.com/MH.liton.360" className="hover:text-teal-400"><FaFacebookF /></a>
+                        <a href="#" className="hover:text-teal-400"><FaXTwitter /></a>
+                        <a href="https://github.com/MH-liton-360" className="hover:text-teal-400"><FaGithub /></a>
+                        <a href="#" className="hover:text-teal-400"><FaInstagram /></a>
+                    </div>
                 </div>
-            </nav>
-        </footer >
+
+
+                <div className='px-15'>
+                    <h3 className="text-lg font-semibold mb-4">Useful Links</h3>
+                    <ul className="space-y-2">
+                        <li><a href="#" className="hover:text-teal-400">Home</a></li>
+                        <li><a href="#" className="hover:text-teal-400">About us</a></li>
+                        <li><a href="#" className="hover:text-teal-400">car-communication</a></li>
+                        <li><a href="#" className="hover:text-teal-400">Founder</a></li>
+                        <li><a href="#" className="hover:text-teal-400">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-lg font-semibold mb-4">Drop a Message</h3>
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="w-1/2 bg-white h-8 rounded-lg p-5 text-black"
+                    />
+                    <br />
+                    <div className='py-3'>
+                        <button className="w-1/2 bg-[#0E7A81] h-9 rounded-lg hover:bg-teal-700 px-2">
+                            Subscribe
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
